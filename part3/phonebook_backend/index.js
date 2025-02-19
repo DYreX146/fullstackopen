0b1f826e5e8a4a705f23855ad079339352a693d8
Taine Wirth <tainewirth@gmail.com>
2025-02-19 05:07:01 +0200

    Add phonebook_backend

---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+morgan.token('body', request => {
+    return request.method === 'POST' ? JSON.stringify(request.body) : null
+})
+
+const app = express()
+
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(cors())
+
+let persons = [
+    {
+        id: '1',
+        name: 'Arto Hellas',
+        number: '040-123456'
+    },
+    {
+        id: '2',
+        name: 'Ada Lovelace',
+        number: '39-44-5323523'
+    },
+    {
+        id: '3',
+        name: 'Dan Abramov',
+        number: '12-43-234345'
+    },
+    {
+        id: '4',
+        name: 'Mary Poppendieck',
+        number: '39-23-6423122'
+    }
+]
+
+app.get('/api/persons', (_request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+
+    if (!person.name || !person.number) {
+        return response.status(400).json({
+            error: 'Person must include name and number properties'
+        })
+    }
+
+    if (persons.map(person => person.name).includes(person.name)) {
+        return response.status(400).json({
+            error: 'Name must be unique'
+        })
+    }
+
+    person.id = String(Math.floor(Math.random() * Math.pow(2, 32)))
+
+    persons.push(person)
+
+    response.json(person)
+})
+
+app.get('/info', (_request, response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`)
+})
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
